a8c0e60db5da4554dca718e042e5a560c87ccf84
benji.benji <benwbethell@gmail.com>
2025-07-09 10:25:08 +0000

    added pretrained models to .gitignore

--- urbansounds.py
+++ urbansounds.py
@@ -1,15 +1,6 @@
 from speechbrain.inference.classifiers import EncoderClassifier
 
-classifier = EncoderClassifier.from_hparams(
-    source="speechbrain/urbansound8k_ecapa",
-    savedir="pretrained_models/gurbansound8k_ecapa",
-)
 
-out_prob, score, index, text_lab = classifier.classify_file(
-    "speechbrain/urbansound8k_ecapa/dog_bark.wav"
-)
-
-print(out_prob, score, index, text_lab)
 
 
 
